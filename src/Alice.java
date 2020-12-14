bec765731af43ad5f9fe6cd6f9f23227733c7916
mikami-yua <lovejiaxi_1@163.com>
2020-12-14 23:15:28 +0800

    Initial commit

---
+++ src/Alice.java
@@ -0,0 +1,13 @@
+public class Alice {
+    private int p;
+    private int q;
+
+    public Alice(int p, int q) {
+        this.p = p;
+        this.q = q;
+    }
+
+    public int mult(int a, int b){
+        return this.p*this.q;
+    }
+}
---
+++ src/Bob.java
@@ -0,0 +1,16 @@
+public class Bob {
+    public void gcd(){
+        /*
+        Scanner scanner = new Scanner(System.in);
+        BigInteger x = scanner.nextBigInteger();//可以接收int类型
+        BigInteger y = scanner.nextBigInteger();//也可以利用BigInteger.valueOf()将其他类型转换为BigInteger类型
+
+        BigInteger gcd = x.gcd(y);	//BigInteger自带的求最大公约数的方法
+
+        //BigInteger.valueOf()使用演示
+        int a = 10;
+        BigInteger b = BigInteger.valueOf(a);
+        
+         */
+    }
+}
